8f00f32e2184044aaf2034bf36ae29ceb406e560
petrmaca <maca@fzp.czu.cz>
2020-08-18 14:24:20 +0200

    Man for params setting

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -247,7 +247,14 @@ setParamsToOnedHru <- function(dHRUM_ptr, ParsVec, ParsNames, singleHruId) {
 #' setInputsToAlldHrus(dHRUM_ptr = dhrus, filname2)
 #' ParDF = data.frame( B_SOIL = 1.6, C_MAX = 100, B_EVAP = 2,  KS = 0.1, KF = 0.2, ADIV = 0.3, CDIV = 0.03,
 #' SDIV = 0.03, CAN_ST = 2, STEM_ST = 2, CSDIV = 0.3, TETR = 5, DDFA = 0.5, TMEL = 0, RETCAP = 10 )
-#' setParamsToOnedHru(dHRUM_ptr = dhrus,as.numeric(ParDF[1,]),names(ParDF),0)
+#' parsvec=as.numeric(ParDF[1,])
+#' for(i in 1:(nHrus-1)){
+#' ParDF <- rbind(ParDF,parsvec)
+#' }
+#' ParDF[,1] <- seq(1:nHrus)
+#' setParsToDistdHRUM(dhrus, ParDF, TRUE)
+#' ParDF[,2] <- seq(1:nHrus)
+#' setParsToDistdHRUM(dhrus, ParDF, FALSE)
 setParsToDistdHRUM <- function(dHRUM_ptr, ParsDF, PrintPars) {
     invisible(.Call(`_dHRUM_setParsToDistdHRUM`, dHRUM_ptr, ParsDF, PrintPars))
 }
