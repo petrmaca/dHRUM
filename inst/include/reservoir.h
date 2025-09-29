336b21322caada1276c22d23225d0d0fa3a807f2
Ludek Bures <buresl@fzp.czu.cz>
2025-09-29 11:07:01 +0200

    Dam (reservoir) instances

--- inst/include/reservoir.h
+++ inst/include/reservoir.h
@@ -1,1 +1,52 @@
+#ifndef RESERVOIR_H
+#define RESERVOIR_H
 
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+#include "numberSel.h"
+#include "data_dam_1d.h"
+
+class dam {
+public:
+  dam();
+  ~dam();
+  dam(const dam& other);
+  dam& operator=(const dam& other);
+
+  numberSel get_dta(const unsigned& tst, const ts_type& _tsType);//!< Getter data on HB of single pdm unit
+  void set_data_prec_temp(const hdata& _prec_dta,const hdata& _temp_dta);//!< Setter data on HB of single pdm unit
+  void set_data(const hdata& dta,const ts_type&_tsType);//!< Setter data on HB of single pdm unit
+  void set_varValue(const numberSel& dta,const  unsigned& tst,const ts_type& _tsType);
+
+  void set_calender();
+
+
+
+protected:
+
+private:
+  data_dam_1d hyd_dta;//!< The data of all time series of hydrological variables
+
+  numberSel prev_Stor;//!< The helper variable for updating reservoir storage
+
+
+  numberSel Area;//!< The area of reservoir unit in m2
+
+
+
+
+
+};
+
+#endif // RESERVOIR_H
--- inst/include/reservoirSel.h
+++ inst/include/reservoirSel.h
@@ -1,1 +1,31 @@
+#ifndef DAMSEL_H
+#define DAMSEL_H
 
+#include <iomanip>
+#include <limits>
+#include <string>
+#include <map>
+
+//water surface evaporation variants
+enum class ETdam_type {ETdam1, ETdam2};
+const ETdam_type all_ETdam[]{ ETdam_type::ETdam1,ETdam_type::ETdam2};
+const std::vector<std::string> allETdamTypeNames {"ETpond1, ETpond2"};
+
+//soil percolation variants
+enum class DamSOISPerc_type {noDamSOISPerc, DamSOISPerc1, DamSOISPerc2, DamSOISPerc3};
+const DamSOISPerc_type all_DamSOISPerc[]{ DamSOISPerc_type::noDamSOISPerc,DamSOISPerc_type::DamSOISPerc1,DamSOISPerc_type::DamSOISPerc2,DamSOISPerc_type::DamSOISPerc3};
+const std::vector<std::string> allDamSOISPercTypeNames {"noDamSOISPerc","DamSOISPerc1", "DamSOISPerc2", "DamSOISPerc3"};
+
+//groundwater percolation variants
+enum class DamGWPerc_type {noDamGWPerc,DamGWPerc1, DamGWPerc2,DamGWPerc3};
+const DamGWPerc_type all_DamGWPerc[]{ DamGWPerc_type::noDamGWPerc,DamGWPerc_type::DamGWPerc1,DamGWPerc_type::DamGWPerc2,DamGWPerc_type::DamGWPerc3};
+const std::vector<std::string> allDamGWPercTypeNames {"noDamGWPerc","DamGWPerc1", "DamGWPerc2","DamGWPerc3"};
+
+//regular out variants
+enum class DamRouT_type {noDamRouT,DamRouT1, DamRouT2,DamRouT3};
+const DamRouT_type all_DamRouT[]{ DamRouT_type::noDamRouT,DamRouT_type::DamRouT1,DamRouT_type::DamRouT2,DamRouT_type::DamRouT3};
+const std::vector<std::string> allDamRouTNames {"noDamRouT","DamRouT1", "DamRouT2","DamRouT3"};
+
+
+
+#endif // DAMSEL_H
